1a0102de3e080e67557aff244507b5c60404fef4
Akumas_Coffin_ <68540569+AkumasCoffin@users.noreply.github.com>
2025-08-10 14:11:44 +1000

    Update rcon.js

--- app/rcon.js
+++ app/rcon.js
@@ -1,1 +1,15 @@
+const { Rcon } = require('rcon-client');
+
+async function sendRconCommand(cmd) {
+    const rcon = await Rcon.connect({
+        host: process.env.RCON_HOST,
+        port: parseInt(process.env.RCON_PORT),
+        password: process.env.RCON_PASSWORD
+    });
+    const response = await rcon.send(cmd);
+    await rcon.end();
+    return response;
+}
+
+module.exports = { sendRconCommand };
 
